469738bb443a52cb8817aae3e355c38f9bcb6d3a
esk <emseely@gmail.com>
2020-08-04 17:51:42 -0400

    stock api route

---
+++ server/api/stock.js
@@ -0,0 +1,87 @@
+const router = require('express').Router
+const {Stock} = require('../db')
+
+router.get('/', async (req, res, next) => {
+  try {
+    const stock = await Stock.findAll()
+    res.json(stock)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const stock = await Stock.findByPk(req.params.id)
+    res.json(stock)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const {
+      name,
+      category,
+      sex,
+      price,
+      quantity,
+      imageUrl,
+      description,
+    } = req.body
+    const newStock = await Stock.create({
+      name,
+      category,
+      sex,
+      price,
+      quantity,
+      imageUrl,
+      description,
+    })
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const {
+      name,
+      category,
+      sex,
+      price,
+      quantity,
+      imageUrl,
+      description,
+    } = req.body
+
+    const stock = await Stock.findByPk(req.params.id)
+
+    await stock.update({
+      name,
+      category,
+      sex,
+      price,
+      quantity,
+      imageUrl,
+      description,
+    })
+
+    res.status(201).json(stock)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const stock = await Stock.findByPk(req.params.id)
+    await stock.destroy()
+    res.status(204).send('deleted')
+  } catch (err) {
+    next(err)
+  }
+})
+
+module.exports = router
