6447c626a76aabff775ca3b8cab407dedcd41431
natalie_ng <natalie.ng.14755@gmail.com>
2020-08-05 11:58:50 -0400

    added order model edits

--- server/api/users.js
+++ server/api/users.js
@@ -1,32 +1,32 @@
-const router = require('express').Router()
-const {User, Order} = require('../db')
+// const router = require('express').Router()
+// const {User, Order} = require('../db')
 
-// for admin to view all users
-router.get('/', async (req, res, next) => {
-  try {
-    const allUsers = await User.findAll()
-    res.send(allUsers)
-  } catch (error) {
-    next(error)
-  }
-})
+// // for admin to view all users
+// router.get('/', async (req, res, next) => {
+//   try {
+//     const allUsers = await User.findAll()
+//     res.send(allUsers)
+//   } catch (error) {
+//     next(error)
+//   }
+// })
 
-//to view single user, for admin and individual user to view info
-router.get('/:id', async (req, res, next) => {
-  try {
-    const singleUser = await User.findAll({
-      where: {
-        id: req.params.id
-      },
-      include: {
-        model: Order
-      }
-    })
-    res.send(singleUser)
-  } catch (error) {
-    next(error)
-  }
-})
+// //to view single user, for admin and individual user to view info
+// router.get('/:id', async (req, res, next) => {
+//   try {
+//     const singleUser = await User.findAll({
+//       where: {
+//         id: req.params.id
+//       },
+//       include: {
+//         model: Order
+//       }
+//     })
+//     res.send(singleUser)
+//   } catch (error) {
+//     next(error)
+//   }
+// })
 
 //for users to checkout
 // router.post('/:id/checkout', async (req, res, next) => {
