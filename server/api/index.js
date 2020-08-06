4f6cf7c3bc7c282123cf93d75b1f98e042c02615
esk <emseely@gmail.com>
2020-08-06 18:01:13 -0400

    chore: remove mentions of cart

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
-router.use('/cart', require('./cart'))
+
 router.use('/order', require('./order'))
 router.use('/product', require('./product'))
 
