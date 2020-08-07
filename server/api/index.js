72dc31cb214b7e647fbb2904e961165de9e43089
Liz John <Sasabonsam@gmail.com>
2020-08-07 10:26:40 -0400

    chore: removed mystery text in server/api/index.js

--- server/api/index.js
+++ server/api/index.js
@@ -2,10 +2,6 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
-<<<<<<< HEAD
-=======
-
->>>>>>> aca7a68816cbc11003ed5ed50b837e248c6bd225
 router.use('/order', require('./order'))
 router.use('/product', require('./product'))
 
