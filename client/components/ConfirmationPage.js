752606622b48252d7d156d140073a5137f402804
lialjohn <sasabonsam@gmail.com>
2020-08-09 20:50:59 +0100

    feat: the cart is now fully operational *vader noises*

---
+++ client/components/ConfirmationPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ConfirmationPage = () => {
+  return (
+    <div>
+      <h3>Thank you for your order!</h3>
+      <p
+      >{`You will recieve a confirmation email shortly\nOr, you would if this was a fully functional e-commerce website :')`}</p>
+    </div>
+  )
+}
+
+export default ConfirmationPage
