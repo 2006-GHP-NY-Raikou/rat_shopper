ff406750565911fe7243421a09100e0ee5bd46e6
natalie_ng <natalie.ng.14755@gmail.com>
2020-08-09 23:24:59 -0400

    feat: add to cart works from singleProduct view

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {fetchSingleProduct} from '../store/singleProduct'
+import {addToCart, addToUserCart} from '../store/cart'
 
 export const SingleProductView = props => {
   let product = props.product
@@ -9,17 +10,43 @@ export const SingleProductView = props => {
       <h1>{product.name}</h1>
       <h2>{product.price}</h2>
       <img src={product.imageUrl} />
+      <div>
+        <button type="button" onClick={props.handleSubmit}>
+          Add
+        </button>
+      </div>
     </div>
   )
 }
 
 export class SingleProduct extends React.Component {
+  constructor() {
+    super()
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
   componentDidMount() {
     try {
       const productId = this.props.match.params.productId
       this.props.loadSingleProduct(productId)
     } catch (err) {
       console.log(err)
+    }
+  }
+
+  handleSubmit() {
+    console.log('hi')
+    console.log(this.props)
+    const product = {
+      productId: this.props.match.params.productId,
+      qty: 1,
+      price: this.props.product.price
+    }
+    console.log(product)
+    if (this.props.user) {
+      this.props.addToUserCart(product)
+    } else {
+      this.props.addToCart(product)
     }
   }
 
@@ -30,19 +57,24 @@ export class SingleProduct extends React.Component {
       return <div>Aw, rats! This product was not found!</div>
     }
 
-    return <SingleProductView product={product} />
+    return (
+      <SingleProductView product={product} handleSubmit={this.handleSubmit} />
+    )
   }
 }
 
 const mapState = state => {
   return {
+    user: state.user,
     product: state.product
   }
 }
 
 const mapDispatch = dispatch => {
   return {
-    loadSingleProduct: id => dispatch(fetchSingleProduct(id))
+    loadSingleProduct: id => dispatch(fetchSingleProduct(id)),
+    addToCart: item => dispatch(addToCart(item)),
+    addToUserCart: item => dispatch(addToUserCart(item))
   }
 }
 
