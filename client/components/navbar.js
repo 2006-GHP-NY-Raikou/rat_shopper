667debf2b2c00040f4b0c4faa2c1d417fd7b30a5
esk <emseely@gmail.com>
2020-08-10 09:10:59 -0400

    fix: undo duplicated navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -39,16 +39,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
             <Link to="/products" exact="true">
               All Products
             </Link>
-            <Link to="/cart" exact="true">
-              Cart
-            </Link>
           </div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/products" exact="true">
-            All Products
-          </Link>
         </div>
       )}
     </nav>
