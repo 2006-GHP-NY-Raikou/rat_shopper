97c3fb80a9062c98f6aab1ddb97ea38f35f77564
ceciliachung <ceciliayu.chung@gmail.com>
2020-08-11 22:32:11 -0700

    style: icon library added and icons added to footer

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {GoMarkGithub} from 'react-icons/go'
+import {AiOutlineLinkedin} from 'react-icons/ai'
 
 const Footer = () => (
   <div id="footer">
@@ -8,19 +10,23 @@ const Footer = () => (
         Your one-stop shop for adopting pet rats and dressing them to the nines!
       </h3>
       <div>
-        <a href="https://github.com/2006-GHP-NY-Raikou/rat_shopper">GitHub</a>
+        <a href="https://github.com/2006-GHP-NY-Raikou/rat_shopper">
+          <GoMarkGithub color="##e36397" size={32} /> GitHub
+        </a>
       </div>
     </div>
     <div className="footerHalf">
       <a href="https://www.linkedin.com/in/cecilia-yu-chung-chang/">
-        Cecilia Yu Chung Chang
+        <AiOutlineLinkedin size={25} /> Cecilia Yu Chung Chang
       </a>
       <a href="https://www.linkedin.com/in/elizabeth-john-630b761a7/">
-        Liz John
+        <AiOutlineLinkedin size={25} /> Liz John
       </a>
-      <a href="https://www.linkedin.com/in/natalie-ng-a812a811b/">Natalie Ng</a>
+      <a href="https://www.linkedin.com/in/natalie-ng-a812a811b/">
+        <AiOutlineLinkedin size={25} /> Natalie Ng
+      </a>
       <a href="https://www.linkedin.com/in/emma-seely-katz-b4386a1a3/">
-        Emma Seely-Katz
+        <AiOutlineLinkedin size={25} />Emma Seely-Katz
       </a>
       <h2>August 2020</h2>
     </div>
